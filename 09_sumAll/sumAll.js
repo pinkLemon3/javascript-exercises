443dac7ef25f46e428f57ee9d1b6fec834500f23
pinkLemon3 <meriblu1211@gmail.com>
2025-11-07 18:24:16 +0000

    determine the min and max values, use a for loop to create an array of the min and max and the numbers inbetween, use forEach loop to add each number to total

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let total = 0;
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let numbers = [];
+    for (let i = min; i <= max; i++) {
+        numbers.push(i);
+    }
+    numbers.forEach(number => {
+        total += number;
+    });
+    return total;
 };
 
 // Do not edit below this line
